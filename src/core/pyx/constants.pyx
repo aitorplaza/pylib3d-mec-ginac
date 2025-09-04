d23ae0ac71effd637978237a720db776702232df
aitorplaza <aitor.plaza@unavarra.es>
2025-09-04 16:37:01 +0200

    Changed how Tau is computed

--- src/core/pyx/constants.pyx
+++ src/core/pyx/constants.pyx
@@ -11,7 +11,7 @@ Description: This module defines symbolic mathematical constants
 # Symbolic constants
 Pi = _expr_from_c(c_ex(c_sym_pi))
 Euler = _expr_from_c(c_ex(c_sym_euler))
-Tau = 2 * Pi
+Tau = Expr(2) * Pi
 
 # Numeric constants
 pi = math.pi
